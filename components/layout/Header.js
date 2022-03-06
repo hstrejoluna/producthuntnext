8ff4f966401fdd5f15f2624993a8e44703affe0b
hstrejoluna <trejolunatutoriales@gmail.com>
2022-03-06 15:38:45 -0500

    Header first setup

---
+++ components/layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div>
+          <p>P</p>
+          {/* Search bar Here */}
+
+          {/* Nav here */}
+        </div>
+
+        <div>{/* Admin menu here */}</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
