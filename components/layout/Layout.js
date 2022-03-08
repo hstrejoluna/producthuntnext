e79fefbf33434c630fc4847a678349535ec3b982
hstrejoluna <trejolunatutoriales@gmail.com>
2022-03-08 11:06:22 -0500

    Added GFonts & Normalize CSS

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Header from "./Header";
 import { Global, css } from "@emotion/react";
+import Head from "next/head";
 
 const Layout = (props) => {
   return (
@@ -42,6 +43,24 @@ const Layout = (props) => {
           }
         `}
       />
+      <Head>
+        <html lang="eng" />
+        <title>Product Hunt Firebase and Next.js </title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
 
       <Header />
 
